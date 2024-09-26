2d441e105bf3f4634cb096f253889efc8162cc27
maimoona <maimoonaaltaf7@gmail.com>
2024-09-26 03:00:32 -0700

    Array chap

---
+++ app.js
@@ -0,0 +1,97 @@
+// Chap - 14-16    [ ARRAY ]
+
+// Q-1
+
+// var studentName = [] ;
+// console.log ( studentName );
+
+
+
+// Q-2
+
+// var Array = [];
+// console.log (Array)
+
+
+// Q-3
+
+
+// var stringArr = ["Hello world"]
+// console.log (stringArr);
+
+
+// // Q-4
+
+// var numArr = [10, 20, 30, 40, 50]
+// console.log ( numArr );
+
+
+// // Q-5
+
+// var boleanArr = [true, false, undefined]
+// console.log (boleanArr);
+
+
+// // Q-6
+
+// var mixedValue = [100, 200, "Hi! how are you ..", true, undefined, 60];
+// console.log (mixedValue);
+
+
+// // Q-7
+
+// var qualification = [  "1) SSC" + "<br>"  +"2) HSC" + "<br>" + "3)BCS" + "<br>" + "4) BS" + "<br>" + "5) BCOM" + "<br>" + "6) MS" + "<br>" + "7) M.Phill." + "<br>" + "8) PhD" ]
+// document.write (qualification)
+
+
+// Q-8
+
+
+// var studentScore = [ "Score of Michel is 320.. percentage : 64%" + "<br>" + "Score of john is 230.. percentage : 46%" + "<br>" + "Score of Tony is 480.. percentage : 96% " ] 
+// document.write (studentScore)
+
+
+// Q-9
+
+// var program = [ "Score of students : 320,230,480,120" + "<br>" + "Ordered scores of students : 120,230,320,480 " ]
+// document.write (program)
+
+
+
+// Q-10
+
+
+
+
+// Q-11
+
+// var city = prompt ( "Enter your city" );
+// var cities = [ "Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar" ]
+// var  flags = 0
+
+// if (city === cities [2]) {
+//     console.log ( "welcome user... ")
+// }
+
+// else if ( city === cities [3] ) {
+//     console.log ("Welcome user")
+//     flags = true
+// }
+
+// else {
+//     console.log ( "you lived in " + cities )
+//     flags === 0
+// }
+
+
+
+// Q-12
+
+// var array = [ "This", "is", "my", "cat" ]
+// console.log (array);
+
+
+// Q-13
+
+var Computer = ["Devices:" + "<br>" + "keyboard " , "mouse ","printer ", "moniter ", +  "<br>" + "<br>" + "Out :" + "<br>" + "Keyboard" + "<br>" + "Out" + "<br>" + "Mouse" +  "<br>" + "Out" +  "<br>" + "printer" +  "<br>" + "out " +  "<br>" ]
+document.write (Computer);
